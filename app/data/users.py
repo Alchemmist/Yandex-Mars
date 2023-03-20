f02f2117d5d3978d18a6934b51f6d2d6ff9812d5
Alchemmist <anton.ingrish@gmail.com>
2023-03-20 04:11:31 +0300

    create news app

--- app/data/users.py
+++ app/data/users.py
@@ -7,9 +7,11 @@ from sqlalchemy import (
             Integer, 
             DateTime,
         )
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -21,3 +23,11 @@ class User(SqlAlchemyBase):
     created_date = Column(DateTime, default=datetime.datetime.now)
 
     news = orm.relationship("News", back_populates='user')
+
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
