d04c86470880848c0f3902e0b9e19268d1e8caf4
Alchemmist <anton.ingrish@gmail.com>
2023-04-28 04:11:47 +0300

    add test for new api

--- app/test_api.py
+++ app/test_api.py
@@ -80,3 +80,21 @@ print(put("http://127.0.0.1:5000/api/jobs/1", json={"id": 999,
 
 print(get("http://127.0.0.1:5000/api/jobs").json())
 """
+
+# TEST 5 --------------------------------
+"""
+print(get('http://localhost:5000/api/v2/users').json())
+print(get('http://localhost:5000/api/v2/users/1').json())
+print(post('http://localhost:5000/api/v2/users', json={'surname': 'a',
+                                                       'name': 'a',
+                                                       'age': 1,
+                                                       'position': 'a',
+                                                       'speciality': 'a',
+                                                       'address': 'a',
+                                                       'email': 'a',
+                                                       'hashed_password': 'a'}).json())
+print(delete('http://localhost:5000/api/v2/users/1').json())
+
+print(get('http://localhost:5000/api/v2/users/999').json())
+print(delete('http://localhost:5000/api/v2/users/999').json())
+"""
